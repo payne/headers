2d4ef4da42523b9bd400480f7d7940218d822c4f
Matt Payne <Payne@MattPayne.org>
2023-02-14 19:19:35 -0600

    http://localhost:8080/headers prints the headers

--- src/main/java/demo/headers/HomeController.java
+++ src/main/java/demo/headers/HomeController.java
@@ -1,8 +1,12 @@
 package demo.headers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
 
 
 @Controller
@@ -14,4 +18,16 @@ public class HomeController {
         return "Hello World!";
     }
 
+    @GetMapping("/headers")
+    @ResponseBody
+    public String headers(HttpServletRequest request) {
+        Enumeration<String> headerNames = request.getHeaderNames();
+        String headerInfo = "<pre>\n";
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            headerInfo += headerName + ": " + request.getHeader(headerName) + "\n";
+        }
+        headerInfo += "</pre>";
+        return headerInfo;
+    }
 }
